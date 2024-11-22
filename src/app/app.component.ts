181edfbfaf78e34e8100733b249035349f05ff99
807782352 <kyrie807782352@gmail.com>
2024-11-22 15:35:10 -0500

    Calculating the Annual Investment Data (Method 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { UserInputComponent } from "./user-input/user-input.component";
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,35 @@ import { UserInputComponent } from "./user-input/user-input.component";
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  // 方式一：计算annual investment data （组件中写）
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    duration: number;
+    annualInvestment: number;
+    expectedReturn: number;
+  }) {
+    const { initialInvestment, duration, annualInvestment, expectedReturn } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
